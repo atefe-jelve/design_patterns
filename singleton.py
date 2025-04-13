9760961458b8af846aeda847e9cae35be3241452
atefe jelve <a.jelve.69@gmail.com>
2025-04-13 09:03:35 +0330

    add singleton design pattern

--- singleton.py
+++ singleton.py
@@ -3,12 +3,12 @@
         - Ensure a class only has one instance, and provide a global point of access to it.
 
 """
-class Singleton:
+class Singleton(type):
     _instance = None
 
     def __call__(self, *args, **kwargs):
         if self._instance is None:
-            self._instance = super().__class__()
+            self._instance = super().__call__()
         return self._instance
 
 
